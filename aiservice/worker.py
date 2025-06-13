015a1c06299f2a39469ffe3536a05a3739b94755
FranklinZEN <wlzeng37@gmail.com>
2025-06-13 11:43:36 -0400

    Include Ai service V0.28

--- aiservice/worker.py
+++ aiservice/worker.py
@@ -7,7 +7,7 @@ import threading
 import json
 import time
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from aiservice.main import background_worker_loop
+from main import background_worker_loop
 
 # Apply asyncio policy patch for Windows if applicable
 if platform.system() == "Windows":
